62fbbd6df232406d0dac6d153506a1bfd0a35c5f
terceiro <terceiro@c25bef26-2b1f-0410-ba83-d4886472e792>
2006-10-09 16:52:34 +0000

    r13@fork:  terceiro | 2006-10-09 13:47:30 -0300  modularizing tape[tape_position]
    git-svn-id: https://intranet.dcc.ufba.br/svn/rturing/trunk@6 c25bef26-2b1f-0410-ba83-d4886472e792

--- turing/machine.rb
+++ turing/machine.rb
@@ -13,17 +13,17 @@ module Turing #:nodoc:
   class Machine
 
     include Observable
-  
+
     attr_accessor :current_state
     attr_accessor :tape
     attr_accessor :tape_position
-  
+
     def setup(input_tape)
       self.tape = input_tape.split(//)
       self.current_state = 0
       self.tape_position = 0
     end
-  
+
     def process
       while(! self.halted)
         changed; notify_observers(self)
@@ -31,20 +31,24 @@ module Turing #:nodoc:
       end
       changed; notify_observers(self)
     end
-  
+
     def current_symbol
       tape[tape_position]
     end
-  
+
+    def current_symbol=(symbol)
+      tape[tape_position] = symbol
+    end
+
     def step
       rule = self.find_rule(self.current_state, self.current_symbol)
       if rule
         self.current_state = rule[:new_state] 
-        self.tape[self.tape_position] = rule[:written_symbol]
+        self.current_symbol = rule[:written_symbol]
         self.tape_position = (rule[:direction] == 'l') ? (tape_position-1) : (tape_position+1)
       end
     end
-  
+
     def initialize(filename = nil)
       if filename
         read(filename)
@@ -52,23 +56,23 @@ module Turing #:nodoc:
         self.states = []
       end
     end
-  
+
     def to_s
       states.map { |s|
         "d(#{s[:original_state]},#{s[:read_symbol]}) = (#{s[:written_symbol]},#{s[:direction]},#{s[:new_state]}) ##{s[:comment]}"
       }.join("\n")
     end
-  
+
     def find_rule(state,symbol)
       states.find { |s|
         s[:original_state] == state && s[:read_symbol] == symbol
       }
     end
-  
+
     def halted
       find_rule(current_state, tape[tape_position]) == nil
     end
-  
+
   end
 
 end
