4ee74112f7e153459d15e06a50ec6668cdd3f930
caio <caio@c25bef26-2b1f-0410-ba83-d4886472e792>
2006-11-14 02:42:37 +0000

    Se ele não altera os butões da mesma forma que o menu, ao menos automatiza e evita duplicatas
    git-svn-id: https://intranet.dcc.ufba.br/svn/rturing/trunk@202 c25bef26-2b1f-0410-ba83-d4886472e792

--- interface/widget/button.rb
+++ interface/widget/button.rb
@@ -1,37 +1,33 @@
 require "gtk2"
 
 class Buttons < Gtk::HBox
-  attr_accessor :botoes,:first,:prev,:stop,:play,:step,:last
+  attr_accessor :buttons,:first,:prev,:stop,:play,:step,:last
   def initialize(window)
     super()
     @actgroup = window.but_actgroup
     proc = Proc.new {|actg, act|
       window.send(act.name)
     }
-     [[Gtk::Stock::GOTO_FIRST, "_Reiniciar"],
-       [Gtk::Stock::GO_BACK, "_Voltar"],
-       [Gtk::Stock::STOP, "_Parar"],
-       [Gtk::Stock::YES, "E_xecutar"],
-       [Gtk::Stock::GO_FORWARD, "Ava_nçar"],
-       [Gtk::Stock::GOTO_LAST, "Últi_mo"]
-     ].each { |id, label|
-      Gtk::Stock.add(id, label)
-    }
-    buttons = [
+    @buttons = [
        ["first", Gtk::Stock::GOTO_FIRST, "_Reiniciar", "", "Retorna ao estado inicial", proc], 
        ["prev", Gtk::Stock::GO_BACK, "_Voltar", "", "Retrocede um passo", proc],
        ["stop", Gtk::Stock::STOP, "_Parar", "", "Interrompe a execução", proc], 
-       ["play", Gtk::Stock::YES, "E_xecutar", "", "Inicia a execução", proc], 
+       ["play", Gtk::Stock::YES, "E_xecutar", "<control>r", "Inicia a execução", proc], 
        ["step", Gtk::Stock::GO_FORWARD, "Ava_nçar", "", "Avança um passo", proc],
        ["last", Gtk::Stock::GOTO_LAST, "Últi_mo", "", "Avança até o último passo ou primeiro loop infinito", proc]]
-    @actgroup.add_actions(buttons)
+    @actgroup.add_actions(@buttons)
+    @buttons.each { |id, stock, label, acc_name|
+      acc = Gtk::Accelerator.parse(acc_name)
+      Gtk::Stock.add(stock, label, acc[0], acc[1])
+    }
+    @accgroup = window.ag
     buttons.each {|but|
       act = @actgroup.get_action(but[0])
       if !act.nil?
-        item = Gtk::Button.new(act.stock_id.to_sym||act.name)
+        button = Gtk::Button.new(act.stock_id.to_sym||act.name)
         act.accel_group = @accgroup
-        act.connect_proxy(item)
-        pack_start(item,false,false,1)
+        act.connect_proxy(button)
+        pack_start(button,false,false,1)
       end
     }
   end
