e7b52001c30d7ace0d5afd3ade520ea18bde9e3b
caio <caio@c25bef26-2b1f-0410-ba83-d4886472e792>
2006-11-14 00:18:06 +0000

    Requerindo mainwindow ao inves de janela principal. Finalizada a renomeacao de JanelaPrincipal
    git-svn-id: https://intranet.dcc.ufba.br/svn/rturing/trunk@192 c25bef26-2b1f-0410-ba83-d4886472e792

--- interface/widget/base.rb
+++ interface/widget/base.rb
@@ -1,6 +1,6 @@
 require "interface/widget/button"
 require "interface/widget/existemerros"
-require "interface/widget/janelaprincipal"
+require "interface/widget/mainwindow"
 require "interface/widget/menus"
 require "interface/widget/radiolist"
 require "interface/widget/salvaantes"
